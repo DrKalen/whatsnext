bc3f82e468544d97c4c1cbd7d67930a52698e175
DrKalen <kalen7@gmail.com>
2017-04-26 20:15:49 +0000

    Added API endpoint to update task completion field to true

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -11,4 +11,11 @@ RSpec.describe TasksController, type: :controller do
                 expect(response_value.count).to eq(2)
             end
     end
+
+    describe "tasks#update" do
+        it "should allow tasks to be marked as done" do
+            task = FactoryGirl.create(:task, done: false)
+            put :update, id:task.id, task: {done:true}
+        end
+    end    
 end
